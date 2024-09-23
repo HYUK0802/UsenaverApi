1488dcf4e0130269c74adafe2f45406f212622e8
hyuk <tig05205@naver.com>
2024-09-23 23:05:56 +0900

    1

---
+++ src/main/java/com/hyuk/naverapi/wishlist/repository/WishListRepository.java
@@ -0,0 +1,11 @@
+package com.hyuk.naverapi.wishlist.repository;
+
+import com.hyuk.naverapi.db.MemoryDbRepositoryAbstract;
+import com.hyuk.naverapi.wishlist.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+
+}
